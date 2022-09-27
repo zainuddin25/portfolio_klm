da17b7510a74eb87e6d049291205fda742a7fafb
zainuddin25 <zanukjepara@gmail.com>
2022-09-27 15:03:03 +0700

    firts commit

---
+++ components/header/index.js
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <div className="w-full bg-white">
+            <div className="container mx-auto border-b-2 py-4 flex justify-between">
+                <h1 className="font-poppins text-3xl font-bold px-8">KLM<span className="text-orange-500">.</span></h1>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
